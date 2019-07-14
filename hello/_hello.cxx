aed679c948f86ed90e70148df8e814adf14b9721
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-07-15 03:52:51 +0900

    Copy example

---
+++ hello/_hello.cxx
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello Python World from C++!!" << std::endl;
+}
---
+++ setup.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from skbuild import setup
+
+setup(name="skbuild-example")
